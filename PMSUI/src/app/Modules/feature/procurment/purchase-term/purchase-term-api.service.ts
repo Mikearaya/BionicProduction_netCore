700dc5af42bc438d8dc04bed2e3b77e6829a7b2a
mikearaya <mikaelaraya12@gmail.com>
2018-12-25 21:01:14 +0300

    completed purchase term service and nswag

--- PMSUI/src/app/Modules/feature/procurment/purchase-term/purchase-term-api.service.ts
+++ PMSUI/src/app/Modules/feature/procurment/purchase-term/purchase-term-api.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { VendorPurchaseTermViewModel, VendorPurchaseTermModel } from 'src/app/Modules/core/DataModels/purchase-terms-data.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseTermApiService {
 
-  constructor() { }
+  private controller = 'procurments/purchaseterms';
+  constructor(private httpClient: HttpClient,
+    @Inject('BASE_URL') private apiUrl: string) {
+
+  }
+
+  getAllPurchaseTerms(id: number): Observable<VendorPurchaseTermViewModel[]> {
+    return this.httpClient.get<VendorPurchaseTermViewModel[]>(`${this.apiUrl}`);
+  }
+
+  updatePurchaseTerm(id: number, updatedPurchaseTerm: VendorPurchaseTermModel): Observable<void> {
+    return this.httpClient.put<void>(`${this.apiUrl}/${this.controller}/${id}`, updatedPurchaseTerm);
+  }
+
+  deletePurchaseTerm(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/${this.controller}/${id}`);
+  }
+
 }
