03ef0f226fb8f1927f368c3cd5868a37500718c1
mikearaya <mikaelaraya12@gmail.com>
2019-01-09 20:03:26 +0300

    added stock git add --all

---
+++ BionicInventory.Application/Inventory/StockBatchs/Commands/Create/MoveStockLotCommandHandler.cs
@@ -0,0 +1,58 @@
+/*
+ * @CreateTime: Jan 9, 2019 7:41 PM
+ * @Author:  Mikael Araya
+ * @Contact: MikaelAraya12@gmail.com
+ * @Last Modified By:  Mikael Araya
+ * @Last Modified Time: Jan 9, 2019 7:59 PM
+ * @Description: Modify Here, Please 
+ */
+using System.Threading;
+using System.Threading.Tasks;
+using Bionic_inventory.Application.Interfaces;
+using BionicInventory.Application.Inventory.StockBatchs.Models;
+using BionicInventory.Application.Shared.Exceptions;
+using BionicInventory.Domain.Storages;
+using BionicProduction.Domain.StockBatchs;
+using MediatR;
+
+namespace BionicInventory.Application.Inventory.StockBatchs.Commands.Create {
+    public class MoveStockLotCommandHandler : IRequestHandler<StockLotMovementDto, uint> {
+        private readonly IInventoryDatabaseService _database;
+
+        public MoveStockLotCommandHandler (IInventoryDatabaseService database) {
+            _database = database;
+        }
+
+        public async Task<uint> Handle (StockLotMovementDto request, CancellationToken cancellationToken) {
+            var mainLotStorage = await _database.StockBatchStorage.FindAsync (request.LotId);
+
+            if (mainLotStorage == null) {
+                throw new NotFoundException (nameof (StockBatch), request.LotId);
+            }
+
+            if (mainLotStorage.Quantity < request.Quantity) {
+                throw new QuantityGreaterThanAvailableException ("Lot", mainLotStorage.Quantity, request.Quantity);
+            }
+
+            var newLot = await _database.StorageLocation.FindAsync (request.newStorageId);
+
+            if (newLot == null) {
+                throw new NotFoundException (nameof (StorageLocation), request.newStorageId);
+            }
+
+            mainLotStorage.Quantity = mainLotStorage.Quantity - request.Quantity;
+
+            _database.StockBatchStorage.Update (mainLotStorage);
+
+            _database.StockBatchStorage.Add (new StockBatchStorage () {
+                StorageId = request.newStorageId,
+                    Quantity = request.Quantity,
+                    BatchId = mainLotStorage.BatchId
+            });
+
+            await _database.SaveAsync ();
+
+            return mainLotStorage.BatchId;
+        }
+    }
+}
